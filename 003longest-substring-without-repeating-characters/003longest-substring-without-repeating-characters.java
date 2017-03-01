0fb2ee77e0825a42133468182bdccd3b9a05ca27
xmzh <1225231425@qq.com>
2017-03-02 00:07:42 +0800

    3 commit

--- 003longest-substring-without-repeating-characters/003longest-substring-without-repeating-characters.java
+++ 003longest-substring-without-repeating-characters/003longest-substring-without-repeating-characters.java
@@ -1,26 +1,73 @@
+//法1
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0)
-          return 0;
-        int start=0;
-        int end=0;
-        int maxLen=0;
-        HashMap<Chaacter,Integer> charMap=new HashMap<Character,Integer>();
-        while(end<s.length()){
-            if(!charMap.containsKey(s.charAt(end))){
-                charMap.put(s.charAt(end),end);
-                int len=end-start+1;
-                maxLen=Math.max(len,maxLen);
-            }
-            else{
-                int val=charMap.get(s.charAt(end));
-                for(int i=start;i<val+1;i++)
-                   charMap.remove(s.charAt(i));
-                start=val+1;
-                charMap.put(s.charAt(end),end);
-            }
-            end++;
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if (allUnique(s, i, j)) ans = Math.max(ans, j - i);
+        return ans;
+    }
+
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
         }
-        return maxLen;
+        return true;
     }
 }
+
+//法2
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
+
+//法3
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
+//法4
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+}
+
--- 003longest-substring-without-repeating-characters/003longest-substring-without-repeating-characters.py
+++ 003longest-substring-without-repeating-characters/003longest-substring-without-repeating-characters.py
@@ -1,3 +1,71 @@
+#法1
+def allUnique(s,start,end):
+        a=set()
+        for i in range(start,end):
+            ch = s[i]
+            if (ch in a):
+                return False
+            a.add(ch)
+        return True
+
+
+class Solution(object):
+    
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        ans = 0
+        for i in range(n):
+            for j in range(i+1,n+1):
+                if (allUnique(s, i, j)):
+                    ans = max(ans, j - i)
+        return ans
+
+
+#法2
+class Solution(object):
+    
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        a=set()
+        ans = 0
+        i=0
+        j=0
+        while i<n and j<n:
+            if((s[j] not in a)):
+                a.add(s[j])
+                j+=1
+                ans = max(ans, j - i)
+            else:
+                a.remove(s[i])
+                i+=1
+        return ans
+#法3
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        amap={}
+        ans = 0
+        i=0
+        for j in range(n):
+            if amap.get(s[j],0)!=0:
+                i=max(amap[s[j]],i)
+            ans = max(ans, j - i + 1)
+            amap[s[j]]=j+1
+        return ans
+    
+#法4    
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
