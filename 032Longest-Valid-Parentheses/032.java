8db8d8185946425abbee0f7b4c9defcfed25efcc
xmzh <1225231425@qq.com>
2017-03-13 10:22:53 +0800

    first commit

---
+++ 032Longest-Valid-Parentheses/032.java
@@ -0,0 +1,29 @@
+//法1：暴力
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+	     //stack.peek()只返回栈顶的值，不删掉栈顶元素
+            } else if (!stack.empty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+    public int longestValidParentheses(String s) {
+        int maxlen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 2; j <= s.length(); j+=2) {
+                if (isValid(s.substring(i, j))) {
+                    maxlen = Math.max(maxlen, j - i);
+                }
+            }
+        }
+        return maxlen;
+    }
+}
+
