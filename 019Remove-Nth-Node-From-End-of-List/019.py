c545ceb0186b7b31b76ba583c0338b98f5ea5cf3
xmzh <1225231425@qq.com>
2017-03-13 10:16:36 +0800

    first commit

---
+++ 019Remove-Nth-Node-From-End-of-List/019.py
@@ -0,0 +1,49 @@
+#法1
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+    	dummy.next = head
+    	length  = 0
+    	first = head
+    	while (first != None):
+    	    length = length +  1
+    	    first = first.next
+    	length=length -n
+    	first = dummy
+    	while (length > 0):
+        	length = length -1
+        	first = first.next
+    	first.next = first.next.next
+    	return dummy.next
+
+#法2
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        first = dummy
+        second = dummy
+        for i in range( 1,n + 2):
+            first = first.next
+        while (first != None):
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+        return dummy.next
+
